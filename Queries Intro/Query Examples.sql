3aa9c5f9254d99af6909f6c5d560d36b40238a07
M <markpackham1@gmail.com>
2025-03-27 13:35:39 +0000

    Add notes on aggregate functions

--- Queries Intro/Query Examples.sql
+++ Queries Intro/Query Examples.sql
@@ -40,3 +40,41 @@ WHERE steals < 1 AND Assists < 2
 SELECT *
 FROM players
 WHERE height > 200 OR weight > 100
+
+
+-- LIKE --
+SELECT *
+FROM products
+WHERE ProductName LIKE "%Xerox%"
+
+
+-- LIKE "Xerox" will return only the records that have Xerox, with no allowance for spaces or other characters before or after Xerox.
+-- LIKE “% Xerox %”  will return records with characters or spaces either before or after the word Xerox. In this case, the text “MyXerox” would be returned if found in the data.
+-- “% Xerox” means Xerox can follow anything but must be the last text found in the column. “The Xerox” would be returned but “Xerox printer" would not.
+-- "Xerox %" means that Xerox must be the first text in the column. Therefore “A Xerox” would not be returned but “Xerox printer” would.
+-- "X%X" will return all records that begin with an X and end with an X.
+
+-- LIKE 'a_c' will return strings like 'abc', 'ahc' or 'a3c' where the second character can be any character. 
+-- In our data, we could request
+-- LIKE PostalCode '6______' to return all postal codes starting with 6
+-- LIKE PostalCode '6_____5' to return all postal codes starting with 6 and ending in 5
+
+SELECT *
+FROM products
+WHERE ProductName LIKE "%Paper"
+
+SELECT *
+FROM products
+WHERE SubCategory LIKE "%Phones%" AND ProductName LIKE "%Nokia%"
+
+SELECT *
+FROM customers
+WHERE CustomerName LIKE "A%"
+
+-- NOT LIKE --
+
+SELECT *
+FROM customers
+WHERE Segment NOT LIKE "%Consumer%"
+
+SELECT * FROM products WHERE productID NOT LIKE "OFF%"
